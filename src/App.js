532fa6aa35d1df72ccef6cd5749c59fe82808763
alister <alister.rozario@gmail.com>
2019-08-20 13:50:00 +0530

    Router setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Default from './pages/Default';
+import Recipes from './pages/Recipes';
+import SingleRecipe from './pages/SingleRecipe';
+import {BrowserRouter as  Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-      <div>
-          Hello World
-      </div>
+      <Router>
+        <main>
+          {/* navbar Goes here */}
+          <Switch>
+            <Route path = "/" exact component={Home} />
+            <Route path = "/recipes" exact component={Recipes} />
+            <Route path = "/recipes/:id" exact component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>  
+      </Router>
   );
 }
 
